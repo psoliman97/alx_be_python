16abdc5e2f7f307d9d69815b64639427b9922f31
Sully <salmaamoohamedsm@gmail.com>
2025-07-07 02:44:55 +0300

    Update bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+        
+    def display_balance(self):
+        print(f"Current Balance: ${format(self.account_balance, '.2f')}")
