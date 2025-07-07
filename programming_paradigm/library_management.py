772ac3b21bcb53d231edb816fb582020e3766512
Sully <salmaamoohamedsm@gmail.com>
2025-07-07 03:57:23 +0300

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -15,7 +15,7 @@ class Book:
     
     class Library:
         def __init__(self):
-            self.books = []
+            self._books = []
 
         def add_book(self, book):
             self.books.append(book)
